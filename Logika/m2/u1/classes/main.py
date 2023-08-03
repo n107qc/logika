8289061f79af60bd9d43d38e08be31f44c64d722
n108qc <140641232+n107qc@users.noreply.github.com>
2023-08-03 18:00:14 +0300

    Add files via upload

--- Logika/m2/u1/classes/main.py
+++ Logika/m2/u1/classes/main.py
@@ -1,16 +1,18 @@
-a = [
-    1, 
-    2, 
-    ['hi', 'bye'], 
-    5, 
-    6, 
-    7, 
-    [3, 2, 3, [9, 'ququ', True]],
-    {
-        1: 0,
-        'gi': 'fs',
-        -9: False
-    }
-]
+class Button:
+    def __init__(self, title_text, x_num, y_num):
+        self.title = title_text
+        self.x = x_num
+        self.y = y_num
+        self.appearance = True
+    def hide(self):
+        self.appearance = False
+    def show(self):
+        self.appearance = True
+    def print_status(self):
+        print('Дані про віджет:')
+        print(self.title, self.x, self.y, self.appearance)
 
-print(a[7]['gi'])
+ok_button = Button('ok', 100, 100)
+ok_button.print_status()
+ok_button.hide()
+ok_button.print_status()
