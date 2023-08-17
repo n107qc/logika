ed078f744594d525e15f8cd519ff37eaae74032d
n108qc <140641232+n107qc@users.noreply.github.com>
2023-08-17 19:31:06 +0300

    Add files via upload

---
+++ Logika/memory_card/memo___card_layout.py
@@ -0,0 +1,107 @@
+''' Вікно для картки питання '''
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import (
+        QApplication, QWidget, 
+        QTableWidget, QListWidget, QListWidgetItem,
+        QLineEdit, QFormLayout,
+        QHBoxLayout, QVBoxLayout, 
+        QGroupBox, QButtonGroup, QRadioButton,  
+        QPushButton, QLabel, QSpinBox)
+app = QApplication([])
+
+# віджети, які треба буде розмістити:
+# кнопка повернення в основне вікно 
+btn_menu = QPushButton('меню')
+# кнопка прибирає вікно і повертає його після закінчення таймера
+btn_sleep = QPushButton('відпочити')
+btn_Ok = QPushButton('відповісти')
+lb_Question = QLabel('')
+
+box_minutes = QSpinBox()
+box_minutes.setValue(5)
+
+RadioGroupBox = QGroupBox('Варіанти відповідей')
+RadioGroup = QButtonGroup()
+
+rbtn1 = QRadioButton('')
+rbtn2 = QRadioButton('')
+rbtn3 = QRadioButton('')
+rbtn4 = QRadioButton('')
+
+RadioGroup.addButton(rbtn1)
+RadioGroup.addButton(rbtn2)
+RadioGroup.addButton(rbtn3)
+RadioGroup.addButton(rbtn4)
+
+layout_ans1 = QHBoxLayout()
+layout_ans2 = QHBoxLayout()
+layout_ans3 = QHBoxLayout()
+
+
+layout_ans2.addWidget(rbtn1)
+layout_ans2.addWidget(rbtn2)
+layout_ans3.addWidget(rbtn3)
+layout_ans3.addWidget(rbtn4)
+
+layout_ans1.addLayout(layout_ans2)
+layout_ans1.addLayout(layout_ans3)
+
+RadioGroupBox.setLayout(layout_ans1)
+
+AnsGroupBox = QGroupBox("Результат тесту")
+lb_Result = QLabel('')
+lb_Correct = QLabel('')
+
+layout_res = QVBoxLayout()
+layout_res.addWidget(lb_Result, aligment=(Qt.AlignLeft | Qt.AlignTop))
+layout_res.addWidget(lb_Result, aligment=Qt.AliggnHCenter)
+AnsGroupBox.setLayout(layout_res)
+AnsGroupBox.hide()
+
+layout_line1 = QHBoxLayout()
+layout_line2 = QHBoxLayout()
+layout_line3 = QHBoxLayout()
+layout_line4 = QHBoxLayout()
+
+layout_line1.addWidget(btn_menu)
+layout_line1.addStretch(1)
+layout_line1.addWidget(btn_sleep)
+layout_line1.addWidget(box_minutes)
+layout_line1.addWidget(QLabel('хвилини'))
+
+layout_line2.addWidget(lb_Question, alignment=(Qt.AlignmentHCenter | Qt.Aligment))
+
+layout_line3.addWidget(RadioGroupBox)
+layout_line3.addWidget(AnsGroupBox)
+layout_line3.addStretch(1)
+
+layout_line4.addWidget(btn_Ok)
+layout_line4.addStretch(1)
+
+layoyt_card = QVBoxLayout()
+layoyt_card.addLayout(layout_line1, stretch=1)
+layoyt_card.addLayout(layout_line2, stretch=2)
+layoyt_card.addLayout(layout_line3, stretch=8)
+layoyt_card.addStretch(1)
+layoyt_card.addLayout(layout_line4, stretch=1)
+layoyt_card.addStretch(1)
+layoyt_card.setSpacing(5)
+
+# введення кількості хвилин
+# кнопка відповіді "Ок" / "Наступний"
+# текст питання
+
+# Опиши групу перемикачів
+
+# Опиши панень з результатами
+
+# Розмісти весь вміст в лейаути. Найбільшим лейаутом буде layout_card
+
+# Результат роботи цього модуля: віджети поміщені всередину layout_card, який можна призначити вікну.
+def show_result():
+    ''' показати панель відповідей '''
+    pass
+
+def show_question():
+    ''' показати панель запитань '''
+    pass
