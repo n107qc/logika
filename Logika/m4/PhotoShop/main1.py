01eb64a8115f216c214c94cc14cd9fb0868ffdf8
n108qc <140641232+n107qc@users.noreply.github.com>
2023-10-27 20:14:07 +0300

    Add files via upload

--- Logika/m4/PhotoShop/main1.py
+++ Logika/m4/PhotoShop/main1.py
@@ -1,12 +1,16 @@
 import os 
 
+import os
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import (
-    QApplication, QWidget, QLabel, QPushButton,QFileDialog, 
-    QHBoxLayout, QVBoxLayout, QListWidget, QLineEdit, 
-    QTextEdit, QInputDialog, QTableWidget,  QListWidgetItem,
-    QFormLayout, QGroupBox, QButtonGroup, QRadioButton, QSpinBox,QMessageBox)
+    QApplication, QWidget, QTextEdit, QLabel, 
+    QListWidget, QPushButton, QLineEdit, QHBoxLayout, QVBoxLayout, QInputDialog,
+    QTableWidget, QListWidgetItem, QFormLayout, 
+    QGroupBox, QButtonGroup, QRadioButton, QSpinBox,QFileDialog,QAction)
+from PyQt5.QtGui import QKeySequence
+from PIL import Image, ImageFilter
 
+from PyQt5.QtGui import QPixmap,QIcon
 app = QApplication([])
 
 window = QWidget()
@@ -17,7 +21,7 @@ btn_folder = QPushButton('Folderψ(._. )>')
 
 btn_left = QPushButton('Вліво(ಥ _ ಥ)')
 btn_right = QPushButton('Вправоo(〃＾▽＾〃)o')
-btn_flip = QPushButton('Перевернути(⓿_⓿)')
+btn_flip = QPushButton('Перевернутиฅʕ•̫͡•ʔฅ')
 btn_sharp = QPushButton("різкістьლ(╹◡╹ლ)")
 btn_bw = QPushButton('Перетворити в чорнобіле◑﹏◐')
 
@@ -116,13 +120,7 @@ Layout1.addLayout(col1, 1)
 Layout1.addLayout(col2, 4)
 window.setLayout(Layout1)
 
-wordkir = QFileDialog.getExistingDirectory()
-
-print(wordkir)
-
-files_and_folders = os.listdir(wordkir)
-
-print(files_and_folders)
+wordkir = ""
 
 def filter(files):
     result = []
@@ -134,8 +132,51 @@ def filter(files):
 
     return result
 
-print(filter(files_and_folders))
+def showFiles():
+    global wordkir
 
+    wordkir = QFileDialog.getExistingDirectory()
+    files_and_folders = os.listdir(wordkir)
+    
+    #['sunflower.jfif', 'vergil.jpg', 'main1.py']
+    filterered_img = filter(files_and_folders)
+
+    lst_files.clear()
+    lst_files.addItems(filterered_img)
+
+class imageProcessor():
+    def __init__(self):
+        self.filename = None
+        self.original = None
+        self.save_dir = 'Modified/'
+
+    def loadImage(self, filename):
+        self.filename = filename
+        full_path = os.path.join(wordkir, filename)
+        self.original = Image.open(full_path)
+        
+    def show_image(self, path):
+        lb_pic.hide()
+
+        pixmapimage = QPixmap(path)
+        w, h = lb_pic.width(), lb_pic.height()
+
+        pixmapimage = pixmapimage.scaled(w, h, Qt.KeepAspectRatio)
+
+        lb_pic.setPixmap(pixmapimage)
+        lb_pic.show()
+
+def showChosenImage():
+    filename = lst_files.currentItem().text()
+    workimage.loadImage(filename)
+    full_path = os.path.join(wordkir, filename)
+    workimage.show_image(full_path)
+
+workimage = imageProcessor()
+
+lst_files.itemClicked.connect(showChosenImage)
+
+btn_folder.clicked.connect(showFiles)
 window.setLayout(Layout1)
 window.show()
 app.exec()
