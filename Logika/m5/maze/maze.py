2ea359f56390a9f21cb21d968d42c5c0a9739fcd
n108qc <140641232+n107qc@users.noreply.github.com>
2023-11-24 20:13:58 +0200

    Add files via upload

--- Logika/m5/maze/maze.py
+++ Logika/m5/maze/maze.py
@@ -1,4 +1,5 @@
 #створи гру "Лабіринт"!
+from typing import Any
 from pygame import *
 from pygame.transform import scale, flip
 from pygame.image import load
@@ -17,6 +18,32 @@ class GameSprite(sprite.Sprite):
     def reset(self):
         win.blit(self.image,(self.rect.x, self.rect.y))
 
+class Player(GameSprite):
+    def update(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < win_height-80:
+            self.rect.y += self.speed
+        if keys[K_LEFT] and self.rect.x > 5:
+            self.rect.x -= self.speed
+        if keys[K_RIGHT] and self.rect.x < win_width-80:
+            self.rect.x += self.speed
+
+class Enemy(GameSprite):
+    direction = 'left'
+    def update(self):
+        if self.direction == 'left':
+            self.rect.x -= self.speed
+        else:
+            self.rect.x += self.speed
+                         
+        if self.rect.x <= 450:
+            self.direction = 'right'
+        if self.rect.x >= win_width-80:
+            self.direction = 'left'
+
+
 
 win_width = 700
 win_height = 500
@@ -24,11 +51,12 @@ win_height = 500
 win = display.set_mode((win_width, win_height))
 
 bk = scale(load("background.jpg"),(win_width,win_height))
-player = GameSprite("hero.png", 5,win_height - 65, 4)
-villain = GameSprite("cyborg.png", win_width - 150,win_height - 250, 2)
-
+player = Player("hero.png", 5,win_height - 65, 4)
+villain = Enemy("cyborg.png", win_width - 150,win_height - 250, 2)
+treasure = GameSprite("treasure.png", win_width-80, win_height-80, 0)
 
 game = True
+finish = False
 clock = time.Clock()
 FPS = 144
 
@@ -36,16 +64,23 @@ mixer.init()
 mixer.music.load("jungles.ogg")
 mixer_music.play()
 
+print(True)
+print(False)
+
 while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
 
-    win.blit(bk, (0, 0))
-    player.reset()
-    villain.reset()
+    if not finish:
+        win.blit(bk, (0, 0))
+        player.reset()
+        villain.reset()
+        treasure.reset()
 
-
+        player.update()
+        villain.update()
+        
 
     display.update()
     clock.tick(FPS)
