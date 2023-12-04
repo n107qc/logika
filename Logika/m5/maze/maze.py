080747a7c1c12066f377b60cddcd69cf02a12df1
n108qc <140641232+n107qc@users.noreply.github.com>
2023-12-04 17:28:35 +0200

    Add files via upload

--- Logika/m5/maze/maze.py
+++ Logika/m5/maze/maze.py
@@ -62,44 +62,39 @@ class Wall(sprite.Sprite):
     def reset(self):
         wind.blit(self.image, (self.rect.x, self.rect.y))
 
-
-    
-
-
 win_width = 700
 win_height = 500
 
 wind = display.set_mode((win_width, win_height))
 
 bk = scale(load("background.jpg"),(win_width,win_height))
-player = Player("hero.png", 5,win_height - 65, 4)
-villain = Enemy("cyborg.png", win_width - 150,win_height - 250, 2)
+player = Player("hero.png", 5,win_height - 65, 5)
+villain = Enemy("cyborg.png", win_width - 150,win_height - 250, 5)
 treasure = GameSprite("treasure.png", win_width-80, win_height-80, 0)
 
 
-Wall1 = Wall(100, 75, 22, 450)
-Wall2 = Wall(200, 0, 22, 250)
-Wall3 = Wall(200, 320, 22, 80)
-Wall4 = Wall(300, 320, 22, 300)
-Wall5 = Wall(300, 70, 22, 180)
-Wall6 = Wall(400, 0, 22, 250)
-Wall7 = Wall(400, 320, 22, 105)
-Wall8 = Wall(500, 70, 22, 180)
-Wall9 = Wall(500, 320, 22, 300)
-Wall10 = Wall(600, 0, 22, 400)
+Wall1 = Wall(100, 75, 15, 450)
+Wall2 = Wall(200, 0  ,15, 250)
+Wall3 = Wall(200, 320, 15, 80)
+Wall4 = Wall(300, 320,15, 300)
+Wall5 = Wall(300, 70, 15, 180)
+Wall6 = Wall(400, 0,  15, 250)
+Wall7 = Wall(400, 320,15, 105)
+Wall8 = Wall(500, 70, 15, 180)
+Wall9 = Wall(500, 320,15, 300)
+Wall10 = Wall(600, 0, 15, 250)
+walls = [Wall1,Wall2,Wall3,Wall4,Wall5,Wall6,Wall7,Wall8,Wall9,Wall10]
 
 game = True
 finish = False
 clock = time.Clock()
-FPS = 144
+FPS = 60
 
 font.init()
 
 f = font.Font(None, 70)
 win = f.render('MOLODEC', True, (23, 79, 146))
 lose = f.render('NE MOLODEC', True, (69, 12, 99))
-
-
 
 mixer.init()
 mixer.music.load("jungles.ogg")
@@ -110,6 +105,8 @@ kick_sound = mixer.Sound('kick.ogg')
 print(True)
 print(False)
 
+flag  = 'not collide'
+tick = FPS
 while game:
     for e in event.get():
         if e.type == QUIT:
@@ -120,16 +117,21 @@ while game:
         player.reset()
         villain.reset()
         treasure.reset()
-        Wall1.reset()
-        Wall2.reset()
-        Wall3.reset()
-        Wall4.reset()
-        Wall5.reset()
-        Wall6.reset()
-        Wall7.reset()
-        Wall8.reset()
-        Wall9.reset()
-        Wall10.reset()
+        for w in walls:
+            w.reset()
+            if sprite.collide_rect(player,w): 
+                # flag = 'collide'
+                # if tick != 0 and flag == 'collide':
+
+                #     player.rect.x -= 5
+                #     tick-=2
+                # else:
+                #     tick = FPS
+                #     flag = 'not collide'
+                finish = True
+                wind.blit(lose, (200, 200))
+                kick_sound.play()
+
 
         player.update()
         villain.update()
@@ -137,11 +139,12 @@ while game:
             finish = True
             wind.blit(win, (200, 200))
             final_sound.play()
-        
-        if  sprite.collide_rect(player,villain) or sprite.collide_rect(player, Wall1) or sprite.collide_rect(player, Wall2) or sprite.collide_rect(player, Wall3) or sprite.collide_rect(player, Wall4) or sprite.collide_rect(player, Wall5) or sprite.collide_rect(player, Wall6) or sprite.collide_rect(player, Wall7) or sprite.collide_rect(player, Wall8) or sprite.collide_rect(player, Wall9) or sprite.collide_rect(player, Wall10) : 
+        if sprite.collide_rect(player,villain):
             finish = True
             wind.blit(lose, (200, 200))
             kick_sound.play()
+
+        
 
 
     display.update()
