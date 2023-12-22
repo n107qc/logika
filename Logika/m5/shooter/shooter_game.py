bc629a05b4af6e260b5d71b4932258ca0115f76e
n108qc <140641232+n107qc@users.noreply.github.com>
2023-12-22 19:49:14 +0200

    Add files via upload

--- Logika/m5/shooter/shooter_game.py
+++ Logika/m5/shooter/shooter_game.py
@@ -33,7 +33,8 @@ class Player(GameSprite):
             self.rect.x += self.speed
 
     def fire(self):
-        pass
+        bullet = Bullet("bullet.png",self.rect.centerx, self.rect.centery,15,20,10)
+        Bullets.add(bullet)
 
 class Enemy(GameSprite):
     direction = 'left'
@@ -45,7 +46,16 @@ class Enemy(GameSprite):
            self.rect.y=0
            self.rect.x=randint(0, win_width-100)
            lost = lost + 1
+
+
+class Bullet(GameSprite):
+    def update(self):
+        self.rect.y-=self.speed
+        if self.rect.y < 0:
+            self.kill()
            
+
+
                          
 
 win_width = 700
@@ -54,6 +64,8 @@ win_height = 500
 window = display.set_mode((win_width, win_height))
 bk = scale(load("galaxy.jpg"),(win_width,win_height))
 hero = Player("rocket.png", 5,win_height - 100, 80, 100, 5)
+
+Bullets = sprite.Group()
 
 monsters = sprite.Group()
 for i in range(2):
@@ -66,7 +78,8 @@ for i in range(2):
 game = True 
 finish = False
 
-
+clock = time.Clock()
+FPS = 144
 
 mixer.init()
 mixer.music.load("space.ogg")
@@ -76,14 +89,19 @@ mixer.music.set_volume(0.05)
 font.init()
 f = font.SysFont('Arial', 36)
 
-
-clock = time.Clock()
-FPS = 144
-
 while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
+
+        if e.type == KEYDOWN:
+            if e.key == K_SPACE:
+                hero.fire()
+
+
+
+
+
     if not finish:
         window.blit(bk, (0, 0))
         txt_lose = f.render(f'Пропущено:{lost}',True,(255,255,255))
@@ -93,7 +111,9 @@ while game:
         hero.reset() 
         villain.reset()
         monsters.draw(window)
+        Bullets.draw(window)
 
+        Bullets.update()
         monsters.update()
         villain.update()
         hero.update()
